b17d0344a56fa49b2a4430aa305ec8ba1c440912
Natalia <PalejNatalia@gmail.com>
2023-11-24 16:29:31 +0000

    Implemented StudentService Interface - adding RMI to the project

---
+++ src/StudentService.java
@@ -0,0 +1,12 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface StudentService extends Remote {
+	
+	String getAll() throws RemoteException;
+	Student getByID(String id) throws RemoteException;
+    Student getByName(String name) throws RemoteException;
+    void add(Student student) throws RemoteException;
+    void update(Student studentToUpdate, Student updatedStudent) throws RemoteException;
+    void delete(String id) throws RemoteException;
+}
